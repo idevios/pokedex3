dc1cec3d4168eb805138994f7f67ff2a1227770b
theerapan <theerapan_kh@ymail.com>
2016-08-14 18:14:11 +0700

    Added PokeCell View

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Theerapan Khanthigul on 8/14/2559 BE.
+//  Copyright © 2559 iDeviOS. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
