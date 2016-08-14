2369cf06ecec321e329cd15feba038d67499c1c4
theerapan <theerapan_kh@ymail.com>
2016-08-14 18:15:07 +0700

    Added PokeCell View-this work

--- pokedex3/PokeCell.swift
+++ pokedex3/PokeCell.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class PokeCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
 }
