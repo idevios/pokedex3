a24d7932a5a4ab0473c6fc0bafa8d13c0479099a
theerapan <theerapan_kh@ymail.com>
2016-08-14 16:14:04 +0700

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Theerapan Khanthigul on 8/14/2559 BE.
+//  Copyright © 2559 iDeviOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
